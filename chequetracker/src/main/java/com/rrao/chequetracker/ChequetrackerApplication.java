4a8b64c25d6d885b1d0197b82350a1fc36142b58
Rajashree Rao Rao <rajashree.rao@nl.abnamro.com>
2019-11-06 13:04:05 +0100

    intial version

---
+++ chequetracker/src/main/java/com/rrao/chequetracker/ChequetrackerApplication.java
@@ -0,0 +1,13 @@
+package com.rrao.chequetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChequetrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChequetrackerApplication.class, args);
+	}
+
+}
---
+++ chequetracker/src/test/java/com/rrao/chequetracker/ChequetrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rrao.chequetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChequetrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

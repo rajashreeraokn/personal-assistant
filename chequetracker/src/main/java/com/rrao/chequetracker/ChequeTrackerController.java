b4d0d2a35f877656c79481f98febff88ac0f4201
Rajashree Rao Rao <rajashree.rao@nl.abnamro.com>
2019-11-13 11:21:04 +0100

    workin with Map for month

--- chequetracker/src/main/java/com/rrao/chequetracker/ChequeTrackerController.java
+++ chequetracker/src/main/java/com/rrao/chequetracker/ChequeTrackerController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -38,6 +39,12 @@ public class ChequeTrackerController {
       @RequestBody Cheque cheque) {
     chequeTrackerService.updateChequeDetails(chequeNumber, cheque);
   }
+
+  @GetMapping("/account/{accountId}/Cheque/month")
+  public List<Cheque> getChequeIssuedForSelectedMonth(@RequestParam("month") int month, @PathVariable String accountId){
+    return chequeTrackerService.getChequeIssuedForSelectedMonth(month);
+  }
+
 
 
 }
--- chequetracker/src/main/java/com/rrao/chequetracker/ChequeTrackerService.java
+++ chequetracker/src/main/java/com/rrao/chequetracker/ChequeTrackerService.java
@@ -1,8 +1,11 @@
 package com.rrao.chequetracker;
 
+import java.time.Month;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,7 +14,7 @@ public class ChequeTrackerService {
   private Map<String, Cheque> chequeMap = new HashMap();
 
   public void addCheque(Cheque c) {
-    chequeMap.put(c.getChequeNumber(), c);
+    this.chequeMap.put(c.getChequeNumber(), c);
   }
 
   public Map<String, Cheque> getChequeMap() {
@@ -19,8 +22,10 @@ public class ChequeTrackerService {
   }
 
   public void updateChequeDetails(String chequeNumber, Cheque cheque) {
-    chequeMap.put(chequeNumber, cheque);
-
+    this.chequeMap.put(chequeNumber, cheque);
   }
 
+  public List<Cheque> getChequeIssuedForSelectedMonth(int month) {
+  return this.chequeMap.values().stream().filter(x -> x.getDate().getMonthValue()==month).collect(Collectors.toList());
+  }
 }
